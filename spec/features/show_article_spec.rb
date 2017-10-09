793176d554b6c5de048c5859e76bde5b01f15e88
Dimo Trifonov <dimo@izpitai.me>
2017-10-09 17:02:48 +0300

    show links Edit and Delete to owners

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -37,5 +37,21 @@ RSpec.feature 'Show an article' do
 
   end
 
+  scenario 'to owner show Edit and Delete buttons' do
+
+    login_as @john
+
+    visit '/'
+
+    click_link @article.title
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+
+    expect(page).to have_link 'Edit Article'
+    expect(page).to have_link 'Delete Article'
+
+  end
+
 end
 
